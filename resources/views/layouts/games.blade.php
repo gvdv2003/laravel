3bfa696073c1c4971e0909201269cd7389cbd7e6
Geert van der Velden <1079252@hr.nl>
2024-11-01 22:05:25 +0100

    add scc

--- resources/views/layouts/games.blade.php
+++ resources/views/layouts/games.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Your App Name')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> <!-- Voeg hier je CSS-bestand toe -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
